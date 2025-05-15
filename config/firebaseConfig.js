169b73ee702cf087cbc631f337b6f0ea22094995
nestoraima230 <nestoraima_22@alu.uabcs.mx>
2025-05-15 12:32:25 -0700

    Firebase (Parte 1)

---
+++ config/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4jy6tWPZPATYoxcvmDvfYOCs3fRIKygU",
+  authDomain: "reportly-2ab0a.firebaseapp.com",
+  projectId: "reportly-2ab0a",
+  storageBucket: "reportly-2ab0a.firebasestorage.app",
+  messagingSenderId: "11492335753",
+  appId: "1:11492335753:web:0ea4c7415c60977c812658"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
